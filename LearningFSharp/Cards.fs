232d4316a05c4ec658bc1da46974f6e545632e3c
Tom Humphreys <Tom01098Code@gmail.com>
2019-05-09 11:06:57 +0100

    Card value and creation validation.

---
+++ LearningFSharp/Cards.fs
@@ -0,0 +1,18 @@
+﻿module Cards
+
+type CardValue =
+    private
+    | Number of int
+    | Ace
+    | Jack
+    | Queen
+    | King
+
+let create num = 
+    match num with
+    | 1 -> Ok Ace
+    | 11 -> Ok Jack
+    | 12 -> Ok Queen
+    | 13 -> Ok King
+    | _ when num > 1 && num < 11 -> Ok <| Number num
+    | _ -> Error <| sprintf "Number %i is not a valid card number." num
--- LearningFSharp/Program.fs
+++ LearningFSharp/Program.fs
@@ -3,10 +3,25 @@ open LoggingUtility
 open System
 open Parsing
 open FizzBuzz
+open Cards
 
 [<EntryPoint>]
 let main argv =
-    FizzBuzzUpTo 15
+    logObj <| create 0
+    logObj <| create 1
+    logObj <| create 2
+    logObj <| create 3
+    logObj <| create 4
+    logObj <| create 5
+    logObj <| create 6
+    logObj <| create 7
+    logObj <| create 8
+    logObj <| create 9
+    logObj <| create 10
+    logObj <| create 11
+    logObj <| create 12
+    logObj <| create 13
+    logObj <| create 14
 
     // Waiting for a key press before closing the console.
     Console.ReadKey() |> ignore
