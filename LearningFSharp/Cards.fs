b083ad3a2c5908ed8857422d64fc7b778ed67f15
Tom Humphreys <Tom01098Code@gmail.com>
2019-05-10 15:54:50 +0100

    Changed Cards implementation for better domain modelling, added a Deal function.

--- LearningFSharp/Cards.fs
+++ LearningFSharp/Cards.fs
@@ -1,36 +1,32 @@
 ﻿module Cards
 
-// The suit of a playing card.
-type CardSuit =
+type Suit =
     | Clubs
-    | Spades
     | Hearts
+    | Spades
     | Diamonds
 
-// The value of a playing card.
-// This is private so that only valid values
-// can be created through the factory.
-type CardValue =
-    private
-    | Number of int
+type Rank =
     | Ace
+    | Two
+    | Three
+    | Four
+    | Five
+    | Six
+    | Seven
+    | Eight
+    | Nine
+    | Ten
     | Jack
     | Queen
     | King
 
-// A playing card with a value and a suit.
-type Card = { Value:CardValue; Suit:CardSuit }
+type Card = { Suit:Suit; Rank:Rank }
 
-/// Create a card with a given value and suit.
-let create value suit = 
-    match value with
-    | 1 -> Ok { Value = Ace; Suit = suit }
-    | 11 -> Ok { Value = Jack; Suit = suit }
-    | 12 -> Ok { Value = Queen; Suit = suit }
-    | 13 -> Ok { Value = King; Suit = suit }
-    | _ when value > 1 && value < 11 -> Ok <| { Value = Number value; Suit = suit }
-    | _ -> Error <| sprintf "'%i' is not a valid card value" value
+type Deck = Deck of Card list
+type Hand = Hand of Card list
 
-/// Convert a playing card to a string form.
-let AsString {Value = value; Suit = suit} =
-    sprintf "%s of %s" (value.ToString()) (suit.ToString())
+let Deal (Deck deck) =
+    match deck with
+    | head :: tail -> Some (head, Deck tail)
+    | [] -> None
