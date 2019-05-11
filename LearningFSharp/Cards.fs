68fc82d804a8799489626194558316bb783fbacf
Tom Humphreys <Tom01098Code@gmail.com>
2019-05-11 19:55:14 +0100

    Changed model to support empty cases, changed backing store to arrays.

--- LearningFSharp/Cards.fs
+++ LearningFSharp/Cards.fs
@@ -23,28 +23,30 @@ type Rank =
     
 type Card = { Suit:Suit; Rank:Rank }
 
-type Deck = Deck of Card list
-type Hand = Hand of Card list
+type Deck = 
+    | Deck of Card array
+    | EmptyDeck
+
+type Hand = 
+    | Hand of Card array
+    | EmptyHand
 
 /// Create an ordered deck of 52 playing cards.
 let newDeck =
-    let suits = [Clubs; Hearts; Spades; Diamonds]
-    let ranks = [Ace; Two; Three; Four; Five; Six; 
-                Seven; Eight; Nine; Ten; Jack; Queen; King]
+    let suits = [|Clubs; Hearts; Spades; Diamonds|]
+    let ranks = [|Ace; Two; Three; Four; Five; Six; 
+                Seven; Eight; Nine; Ten; Jack; Queen; King|]
 
-    let cards = List.allPairs suits ranks 
-                |> List.map (fun pair -> { Suit = fst pair; Rank = snd pair })
+    let cards = Array.allPairs suits ranks 
+                |> Array.map (fun pair -> { Suit = fst pair; Rank = snd pair })
 
     Deck cards
 
 /// Deal the top card from the deck.
-let deal (Deck deck) =
+let deal deck =
     match deck with
-    | head :: tail -> Some (head, Deck tail)
-    | [] -> None
-
-/// Is the deck empty?
-let isDeckEmpty (Deck deck) = deck.IsEmpty
-
-/// Is the hand empty?
-let isHandEmpty (Hand hand) = hand.IsEmpty
+    | Deck cards ->
+        match cards.Length with
+        | 1 -> (Some cards.[0], EmptyDeck)
+        | _ -> (Some cards.[0], Deck cards.[1..])
+    | EmptyDeck -> (None, EmptyDeck)
