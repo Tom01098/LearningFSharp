a5c85b43446e8490a6922862f8bb6dd58293309d
Tom Humphreys <Tom01098Code@gmail.com>
2019-05-14 20:00:17 +0100

    Built a simple validator using a railway.

--- LearningFSharp/Program.fs
+++ LearningFSharp/Program.fs
@@ -4,10 +4,29 @@ open System
 open Parsing
 open FizzBuzz
 open Cards
+open Railway
+
+let startsWithA (input: string) =
+    if input.[0] = 'A'
+    then Success input
+    else Failure "Does not start with 'A'"
+
+let isLessThan5 input =
+    if String.length input < 5
+    then Success input
+    else Failure "Input is 5+ characters"
+
+let endsWithZ input =
+    let index = String.length input - 1
+    if input.[index] = 'Z'
+    then Success input
+    else Failure "Does not end with 'Z'"
 
 [<EntryPoint>]
 let main argv =
-    logObj <| shuffle newDeck
+    let validator = startsWithA >=> isLessThan5 >=> endsWithZ
+
+    validator "ASdZ" |> logObj
 
     // Waiting for a key press before closing the console.
     Console.ReadKey() |> ignore
---
+++ LearningFSharp/Railway.fs
@@ -0,0 +1,19 @@
+﻿module Railway
+
+type Result<'Success, 'Failure> =
+    | Success of 'Success
+    | Failure of 'Failure
+
+let succeed x = Success x
+let fail x = Failure x
+
+// Call a success or failure function depending on the input.
+let either fSuccess fFailure input =
+    match input with
+    | Success s -> fSuccess s
+    | Failure f -> fFailure f
+
+// Convert a function into a function which accepts a Result,
+// calling the inner function with the success value.
+let convert f = either f fail
+let ( >=> ) f g = f >> convert g
